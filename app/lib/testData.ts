2ec681f897e678b754834f1363668342b9b98333
Ciara Williams <cwil290831@gmail.com>
2025-11-13 15:44:18 -0500

    schemas plus the start of the definitions

---
+++ app/lib/testData.ts
@@ -0,0 +1,22 @@
+const users = [
+    {
+        id:1, 
+        first: "Ciara", 
+        last: "Williams", 
+        "contacts":{
+            id:1
+        }
+    }
+]
+const persons = [
+    {
+        id:1, 
+        first: "Jennifer", 
+        last: "Haggerty",
+        phone: "555-123-4567",
+        photo: "testImage.png",
+        birthdate: "1990-05-15",
+        lastContacted: "2025-10-01",
+        lastDismissed: "2025-09-15",
+    }
+]
