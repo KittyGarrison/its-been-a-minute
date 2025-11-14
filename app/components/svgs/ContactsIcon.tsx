0c8ecabfa8c93b8fe926b1360aee5f236bad5c5e
Jennifer <thejenniferhaggerty@gmail.com>
2025-11-14 14:50:53 -0500

    Opted to create each button individually instead of getting fancy. Colors, icons, text applied, functions ready to be added. Added missing icons and matched file types across svgs

---
+++ app/components/svgs/ContactsIcon.tsx
@@ -0,0 +1,19 @@
+export const ContactsIcon = () => {
+  return (
+    <svg
+      width="800px"
+      height="800px"
+      viewBox="0 0 48 48"
+      id="Layer_2"
+      data-name="Layer 2"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M10.35,4.5a2,2,0,0,0-1.95,2v35.1a2,2,0,0,0,1.95,2h27.3a2,2,0,0,0,2-2V6.45a2,2,0,0,0-2-1.95ZM24,13.27a5.37,5.37,0,1,1-5.36,5.37A5.37,5.37,0,0,1,24,13.27ZM24,26c6,0,10.73,1.67,10.73,3.66v5.12H13.27V29.61C13.27,27.62,18,26,24,26Z" />
+    </svg>
+  );
+};
+
+export default ContactsIcon;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { Card } from "./components/Card";
-
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-emerald-50 to-teal-100 flex items-center justify-center p-4">
-      <Card />
+      Hello World!
     </div>
   );
 }
