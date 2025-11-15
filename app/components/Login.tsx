b7185a20132ac05a5e5a25f870c840f9c03e5ece
Kitty Katrina Garrison <katrinagarrison@Kittys-MacBook-Pro.local>
2025-11-15 05:21:15 -0500

    login page added

---
+++ app/components/Login.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button, Input, Form } from "@heroui/react";
+
+interface LoginProps {
+  loginClick?: () => void;
+}
+
+const Login = ({ loginClick }: LoginProps) => {
+  return (
+    <Form className="flex flex-col gap-4 p-6" onSubmit={loginClick}>
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold">Welcome back</h1>
+        <p className="text-sm text-default-500">Sign in feel connected.</p>
+      </div>
+      <Input
+        errorMessage="Please enter a valid email"
+        label="Email"
+        labelPlacement="outside"
+        name="email"
+        placeholder="Enter your email"
+      />
+      <Input
+        label="Password"
+        labelPlacement="outside"
+        placeholder="••••••••"
+        type="password"
+      />
+      <Button type="submit" aria-label="Login" color="primary" fullWidth>
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default Login;
+
+
+
+
